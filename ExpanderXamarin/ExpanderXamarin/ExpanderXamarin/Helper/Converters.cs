a302d97da6013c8a394f814b6ae6c5f7d599ab88
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-25 15:57:29 +0530

    XAMARIN-38731 - Sample and screenshot added.

---
+++ ExpanderXamarin/ExpanderXamarin/ExpanderXamarin/Helper/Converters.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ExpanderXamarin
+{
+    #region ExpanderIconConverter
+    public class ExpanderIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return ImageSource.FromResource("ExpanderXamarin.Images.ArrowDown.png");
+            else
+                return ImageSource.FromResource("ExpanderXamarin.Images.ArrowUp.png");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+
+    #region CornerRadiusConverter
+    public class CornerRadiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? new CornerRadius(20, 20, 0, 0) : new CornerRadius(20, 20, 20, 20);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    #endregion
+}
